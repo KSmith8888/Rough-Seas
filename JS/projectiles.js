f5f849371fa3c4711ed0c741cd0390249d41f8b4
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-11-06 18:03:14 -0800

    Updated structure and imports/exports

--- JS/projectiles.js
+++ JS/projectiles.js
@@ -1,21 +1,23 @@
-import { player, ui, weapon } from './app.js';
+import { ui } from './user-interface.js';
 
 //Player Projectiles
 
 class Shell {
-    constructor() {
+    constructor(player, weapon) {
+        this.player = player;
+        this.weapon = weapon;
         this.width = 20;
         this.height = 16;
-        this.x = player.x + 120;
-        this.y = player.y - 10;
+        this.x = this.player.x + 120;
+        this.y = this.player.y - 10;
         this.damage = 10;
         this.hit = false;
         this.projectileAnglesX = [-5, -5, 0, 5, 5];
         this.projectileAnglesY = [0, 5, 5, 5, 0];
-        this.projecttileAdjust = weapon.weaponAngle;
+        this.projecttileAdjust = this.weapon.weaponAngle;
         this.shellImageArray = ['Images/Cannon/shellLeft.png', 'Images/Cannon/shellUpLeft.png', 'Images/Cannon/shellUp.png', 'Images/Cannon/shellUpRight.png', 'Images/Cannon/shellRight.png'];
         this.shellImage = new Image(20, 16);
-        this.shellImage.src = this.shellImageArray[weapon.weaponAngle];
+        this.shellImage.src = this.shellImageArray[this.weapon.weaponAngle];
     }
     Draw() {
         ui.ctx.drawImage(this.shellImage, this.x, this.y, this.width, this.height);
@@ -23,28 +25,30 @@ class Shell {
     UpdatePosition() {
         this.x += this.projectileAnglesX[this.projecttileAdjust];
         this.y -= this.projectileAnglesY[this.projecttileAdjust];
-        player.firedProjectiles.forEach((projectile, index) => {
+        this.player.firedProjectiles.forEach((projectile, index) => {
             if(projectile.x > ui.canvas.width || projectile.x < 0 || projectile.y < 0 || projectile.y > ui.canvas.height) {
-               player.firedProjectiles.splice(index, 1);
+                this.player.firedProjectiles.splice(index, 1);
             }
         });
     }
 }
 
 class Bullet {
-    constructor() {
+    constructor(player, weapon) {
+        this.player = player;
+        this.weapon = weapon;
         this.width = 20;
         this.height = 16;
-        this.x = player.x + 130;
-        this.y = player.y - 20;
+        this.x = this.player.x + 130;
+        this.y = this.player.y - 20;
         this.damage = 5;
         this.hit = false;
         this.projectileAnglesX = [-8, -8, 0, 8, 8];
         this.projectileAnglesY = [0, 8, 8, 8, 0];
-        this.projecttileAdjust = weapon.weaponAngle;
+        this.projecttileAdjust = this.weapon.weaponAngle;
         this.bulletImageArray = ['Images/Machine/bulletLeft.png', 'Images/Machine/bulletUpLeft.png', 'Images/Machine/bulletUp.png', 'Images/Machine/bulletUpRight.png', 'Images/Machine/bulletRight.png'];
         this.bulletImage = new Image(20, 16);
-        this.bulletImage.src = this.bulletImageArray[weapon.weaponAngle];
+        this.bulletImage.src = this.bulletImageArray[this.weapon.weaponAngle];
     }
     Draw() {
         ui.ctx.drawImage(this.bulletImage, this.x, this.y, this.width, this.height);
@@ -52,20 +56,22 @@ class Bullet {
     UpdatePosition() {
         this.x += this.projectileAnglesX[this.projecttileAdjust];
         this.y -= this.projectileAnglesY[this.projecttileAdjust];
-        player.firedProjectiles.forEach((projectile, index) => {
+        this.player.firedProjectiles.forEach((projectile, index) => {
             if(projectile.x > ui.canvas.width || projectile.x < 0 || projectile.y < 0 || projectile.y > ui.canvas.height) {
-               player.firedProjectiles.splice(index, 1);
+                this.player.firedProjectiles.splice(index, 1);
             }
         });
     }
 }
 
 class Rocket {
-    constructor() {
+    constructor(player, weapon) {
+        this.player = player;
+        this.weapon = weapon;
         this.width = 20;
         this.height = 16;
-        this.x = player.x + 130;
-        this.y = player.y - 20;
+        this.x = this.player.x + 130;
+        this.y = this.player.y - 20;
         this.damage = 15;
         this.hit = false;
         this.projectileAnglesX = [-3, -3, 0, 3, 3];
@@ -81,9 +87,9 @@ class Rocket {
     UpdatePosition() {
         this.x += this.projectileAnglesX[this.projecttileAdjust];
         this.y -= this.projectileAnglesY[this.projecttileAdjust];
-        player.firedProjectiles.forEach((projectile, index) => {
+        this.player.firedProjectiles.forEach((projectile, index) => {
             if(projectile.x > ui.canvas.width || projectile.x < 0 || projectile.y < 0 || projectile.y > ui.canvas.height) {
-               player.firedProjectiles.splice(index, 1);
+                this.player.firedProjectiles.splice(index, 1);
             }
         });
     }
